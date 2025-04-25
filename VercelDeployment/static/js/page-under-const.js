03a54184465b3cc8833659fce4d3377b16b26ff0
MillicentBluenight <mbluenight69@gmail.com>
2025-04-25 20:04:47 +0800

    added sitemap and pageunder cxonst

---
+++ VercelDeployment/static/js/page-under-const.js
@@ -0,0 +1,4 @@
+//Optional: Redirect to another page after a certain time
+setTimeout(function() {
+    window.location.href = "https://google.com"; // Change to your desired URL
+        }, 5000); // Redirects after 5 seconds
