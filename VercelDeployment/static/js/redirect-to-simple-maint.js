b241cf55b59bc42e0e0c9913b8a46bf017b6eedd
hndrx67 <brentifromph696@gmail.com>
2025-09-02 01:15:00 +0800

    disabled main site

--- VercelDeployment/static/js/redirect-to-simple-maint.js
+++ VercelDeployment/static/js/redirect-to-simple-maint.js
@@ -1,3 +1,3 @@
 setTimeout(function() {
     window.location.href = "simple-maint.html"; // Replace with your desired URL
-  }, 100); // Delay in milliseconds (e.g., 5000ms = 5 seconds)
+  }, 10); // Delay in milliseconds (e.g., 5000ms = 5 seconds)
